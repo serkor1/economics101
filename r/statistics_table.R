dd049c9f2bda2388722579660a2014bca7125a75
Serkan Korkmaz <home@Serkans-MacBook-Air.local>
2021-11-17 13:40:07 +0100

    Added Tax-revenue to the Stattable

--- r/statistics_table.R
+++ r/statistics_table.R
@@ -9,42 +9,128 @@ stat_table <- function(supply, demand, advanced) {
   )
   
   
-  # Calculate PS and CS
-  producer_surplus <- ( intersections$y_int * intersections$x_int ) / 2
-  consumer_surplus <- (( max(demand$y_initial) - intersections$y_int) * intersections$x_int) / 2
-  total_welfare <-  consumer_surplus + producer_surplus
-  price <- intersections$y_int
-  quantity <- intersections$x_int
   
-  
-  
-  
-  
-  
-  # Stats;
-  stat_table <- tibble(
-    price,
-    quantity,
-    consumer_surplus,
-    producer_surplus,
-    total_welfare
+  intersections <- intersection_function(
+    supply = supply,
+    demand = demand
   )
   
   
+  # if the imperfect y intercept
+  # is equal to the intercept of 
+  # of y_demand and y_supply
+  # there is perfect comptetition
   
-  if (isFALSE(advanced)) {
+  
+  isEqual <- setequal(
+    intersections$y_demand,
+    intersections$y_supply
+  )
+  
+  
+  if (isTRUE(isEqual)){
     
     
-    stat_table <- stat_table %>% 
-      select(
-        "price", "quantity"
-      )
+    ps <- (intersections$perfect_yint * intersections$perfect_xint) / 2
+    cs <- ( (max(demand$y_initial) - intersections$perfect_yint) * intersections$perfect_xint)/2
+    tw <- ps + cs
+    c_price  <- intersections$perfect_yint
+    s_price <- intersections$perfect_yint
+    
+    q  <- intersections$perfect_xint
+    
+    
+  } else {
+    
+    # Determine the existence of 
+    # the values. Depending on the shock being on supply side
+    # or demand side determines whether the intervention variable exists
+    if ("y_intervention" %in% colnames(demand)){
+      
+      y_demand <- demand$y_intervention
+      
+      
+    } else {
+      
+      y_demand <- demand$y_initial
+      
+      
+    }
+    
+    if ("y_intervention" %in% colnames(supply)){
+      
+      y_supply <- supply$y_intervention
+      
+      
+    } else {
+      
+      y_supply <- supply$y_initial
+      
+      
+    }
+    
+    
+    
+    ps <- (intersections$imperfect_yint * intersections$imperfect_xint) / 2
+    cs <- ( (max(y_demand) - intersections$imperfect_yint) * intersections$imperfect_xint)
+    tw <- ps + cs
+    c_price  <- intersections$imperfect_yint
+    s_price <- intersections$imperfect_yint
+    
+    q  <- intersections$imperfect_xint
+    
+    
+  }
+  
+  # If the market is taxed,
+  # there is a discrepancy between
+  # the market price, and the price that 
+  # the suppliers are getting
+  
+  if (mean(demand$taxed) == 1) {
+    
+    
+    tax_revenue <- (intersections$y_demand - intersections$y_supply) * intersections$imperfect_xint
+    
+    c_price  <- intersections$y_demand
+    s_price <- intersections$y_supply
+    
+    q  <- intersections$imperfect_xint
+    
+    
+    
+  } else {
+    
+    tax_revenue <- 0
     
   }
   
   
   
-  stat_table  %>% pivot_longer(
+  stat_table <- tibble(
+    consumer_price  = c_price,
+    producer_price = s_price,
+    quantitiy  = q,
+    producer_surplus = ps,
+    consumer_surplus = cs,
+    total_welfare = tw,
+    tax_revenue = tax_revenue
+  )
+  
+  
+  if (isFALSE(advanced)) {
+    
+    stat_table <- stat_table %>% 
+      select(
+        contains("price"),
+        contains("quantitiy")
+      )
+    
+    
+  }
+  
+  
+  stat_table %>% pivot_longer(
     cols = everything(),
     names_to = "Measure",
     values_to = "Value"
